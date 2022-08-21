ed5f4e8f2005d2c743403de519cf2d62dbf9b95d
tiagoschad <tiagoschad@gmail.com>
2022-08-21 12:01:36 -0300

    autenticado com o firebase

--- cadastro.js
+++ cadastro.js
@@ -1,3 +1,18 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA2MR-EIhfrBldyxsg5Hfjl09Iwbof9cyk",
+    authDomain: "projetesvdb.firebaseapp.com",
+    databaseUrl: "https://projetesvdb-default-rtdb.firebaseio.com",
+    projectId: "projetesvdb",
+    storageBucket: "projetesvdb.appspot.com",
+    messagingSenderId: "214663302807",
+    appId: "1:214663302807:web:cbd3ab564500f58d0763d2",
+    measurementId: "G-MRWJK7P0XN"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+db - firebase.database()
+
 function Salvar()
 {
     if ((document.getElementById("brinco").value == '') || (document.getElementById("raca").value == '') || (document.getElementById("idade").value == '') || (document.getElementById("racao").value == '') || (document.getElementById("vacina").value == '') || (document.getElementById("massa").value == ''))
